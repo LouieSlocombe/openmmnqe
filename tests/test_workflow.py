cf77651928f250a535f3de7832a7c1f4ffb18905
Louie Slocombe <louies@hotmail.co.uk>
2026-03-01 14:40:00 -0700

    added test_malonaldehyde_pt_adqtb_solvated

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -446,6 +446,89 @@ def test_malonaldehyde_pt_quantum_solvated():
     nqe.remove_file('plumed.dat')
 
 
+def test_malonaldehyde_pt_adqtb_solvated():
+    print(flush=True)
+    temperature = 300.0 * unit.kelvin
+    steps_prod = 10_000
+
+    input_pdb = 'tests/data/pdb/malonaldehyde.pdb'
+    potential = MLPotential('mace-off23-small')
+
+    rm_ions = ['Na+',
+               'Cl-',
+               'NA']
+    pdb_data, molecule = nqe.prepare_lig_system(input_pdb, rm_ions=rm_ions)
+    modeller = app.Modeller(pdb_data.topology, pdb_data.positions)
+    modeller.deleteWater()
+    modeller.addHydrogens()
+    forcefield = nqe.prepare_ligand_ff(("amber14-all.xml", "amber14/tip3pfb.xml"),
+                                       molecule)
+
+    padding = 1.5
+    box_shape = 'cube'
+    modeller.addSolvent(forcefield,
+                        padding=padding * unit.nanometer,
+                        boxShape=box_shape)
+
+    nqe.center_in_box(modeller)
+
+    chains = list(modeller.topology.chains())
+    ml_atoms = [atom.index for atom in chains[0].atoms()]
+
+    nqe.run_openmm_relaxation_simple(modeller,
+                                     forcefield,
+                                     potential=potential,
+                                     ml_idx=ml_atoms)
+    pdb = app.PDBFile("minimized.pdb")
+    modeller = app.Modeller(pdb.topology, pdb.positions)
+
+    nqe.run_openmm_adqtb_eq(modeller,
+                            forcefield,
+                            platform_name='CUDA',
+                            n_report=1,
+                            steps=100)
+
+    pdb = app.PDBFile("adqtb_ready.pdb")
+    modeller = app.Modeller(pdb.topology, pdb.positions)
+
+    idx = nqe.atom_indices_from_vmd_picks(modeller, ['LIG1:O2', 'LIG1:H5', 'LIG1:O1'])
+    plumed_input, sum_hills_input = nqe.plumed_input_1pt(modeller,
+                                                         idx,
+                                                         temperature,
+                                                         height=8.0,
+                                                         bias=5.0)
+
+    plumed_script_path = "plumed.dat"
+    with open(plumed_script_path, 'w') as f:
+        f.write(plumed_input)
+
+    nqe.run_openmm_adqtb_prod(modeller,
+                              forcefield,
+                              plumed_script_path=plumed_script_path,
+                              platform_name='CUDA',
+                              temperature=temperature,
+                              steps=steps_prod,
+                              potential=potential,
+                              ml_idx=ml_atoms)
+
+    # Run PLUMED sum_hills to get FES
+    os.system(sum_hills_input)
+    nqe.plot_plumed_fes("fes.dat")
+    plt.show()
+
+    nqe.plot_plumed_colvar("COLVAR")
+    plt.show()
+
+    nqe.remove_file_pattern('minimized*')
+    nqe.remove_file_pattern('adqtb_ready*')
+    nqe.remove_file_pattern('adqtb_prod*')
+
+    nqe.remove_file('COLVAR')
+    nqe.remove_file('HILLS')
+    nqe.remove_file('fes.dat')
+    nqe.remove_file('plumed.dat')
+
+
 def test_malonaldehyde_pt_solvated_full():
     print(flush=True)
     temperature = 300.0 * unit.kelvin
@@ -692,7 +775,7 @@ def test_gc_pt_solvated():
 def test_gc_pt_quantum_solvated():
     print(flush=True)
     temperature = 300.0 * unit.kelvin
-    steps_prod = 20_000
+    steps_prod = 30_000
     n_beads = 2
 
     input_pdb = 'tests/data/pdb/gc.pdb'
