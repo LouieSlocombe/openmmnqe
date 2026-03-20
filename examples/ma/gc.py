4a441ade82885a52826c01a968a482be7f92289c
Louie Slocombe <louies@hotmail.co.uk>
2026-03-20 16:08:35 -0700

    clean up GC

--- examples/ma/gc.py
+++ examples/ma/gc.py
@@ -11,7 +11,7 @@ import openmmnqe as nqe
 if __name__ == "__main__":
     print(flush=True)
     temperature = 300.0 * unit.kelvin
-    steps_prod = 10_000
+    steps_prod = 50_000
     input_pdb = os.path.join(os.path.dirname(nqe.openmm_nqe_dir), 'tests/data/pdb/gc.pdb')
     ml_model = 'mace-off23-small'
     forcefield_names = ("amber14-all.xml", "amber14/tip3pfb.xml")
@@ -40,21 +40,20 @@ if __name__ == "__main__":
 
     pdb = app.PDBFile("minimized.pdb")
     modeller = app.Modeller(pdb.topology, pdb.positions)
-    nqe.save_only_index_atoms(modeller, ml_atoms, file_idx='index_atoms.pdb')
-
-    reactant = read('index_atoms.pdb')
-    # view(reactant)
-    product = nqe.swap_bonding_configuration(reactant, 17, 19, 11)
-    product = nqe.swap_bonding_configuration(product, 5, 7, 20)
-    # view(product)
-    calc = mace_off(model_name=ml_model, device='cuda')
-    product = nqe.optimise_geom(product, calc, fmax=0.01)
-    neb_path = nqe.quick_guess_path(reactant, product)
-    write("neb_path.xyz", neb_path)
+    # nqe.save_only_index_atoms(modeller, ml_atoms, file_idx='index_atoms.pdb')
+    #
+    # reactant = read('index_atoms.pdb')
+    # # view(reactant)
+    # product = nqe.swap_bonding_configuration(reactant, 17, 19, 11)
+    # product = nqe.swap_bonding_configuration(product, 5, 7, 20)
+    # # view(product)
+    # calc = mace_off(model_name=ml_model, device='cuda')
+    # product = nqe.optimise_geom(product, calc, fmax=0.01)
+    # neb_path = nqe.quick_guess_path(reactant, product)
+    # write("neb_path.xyz", neb_path)
     # view(neb_path)
-    nqe.convert_xyz_to_plumed_ref("neb_path.xyz", "index_atoms.pdb", "neb_path.pdb")
-
-    plumed_input, sum_hills_input = nqe.plumed_input_neb_path(temperature)
+    # nqe.convert_xyz_to_plumed_ref("neb_path.xyz", "index_atoms.pdb", "neb_path.pdb")
+    # plumed_input, sum_hills_input = nqe.plumed_input_neb_path(temperature)
 
     pdb = app.PDBFile("minimized.pdb")
     modeller = app.Modeller(pdb.topology, pdb.positions)
@@ -74,6 +73,20 @@ if __name__ == "__main__":
     #
     # pdb = app.PDBFile("npt_equilibrate.pdb")
     # modeller = app.Modeller(pdb.topology, pdb.positions)
+
+
+    idx1 = nqe.atom_indices_from_vmd_picks(modeller, ['GGG1:O1', 'CCC1:H2', 'CCC1:N1'])
+    idx2 = nqe.atom_indices_from_vmd_picks(modeller, ['CCC1:N2', 'GGG1:H3', 'GGG1:N3'])
+
+    plumed_input, sum_hills_input = nqe.plumed_input_2pt_1d(modeller,
+                                                            idx1,
+                                                            idx2,
+                                                            temperature,
+                                                            wall=1.0,
+                                                            height=40.0,
+                                                            bias=10.0,
+                                                            f_opes=True)
+
 
     plumed_script_path = "plumed.dat"
     with open(plumed_script_path, 'w') as f:
@@ -101,6 +114,8 @@ if __name__ == "__main__":
     # nqe.remove_file_pattern('prod*')
     nqe.remove_file('COLVAR')
     nqe.remove_file('HILLS')
+    nqe.remove_file('STATE')
+    nqe.remove_file('KERNELS')
     nqe.remove_file('fes.dat')
     nqe.remove_file('plumed.dat')
     nqe.remove_file('index_atoms.pdb')
