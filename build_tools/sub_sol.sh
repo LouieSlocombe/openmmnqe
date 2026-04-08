f70339267812c0c553c4d6a3f3416fc351d2491c
Louie Slocombe <lslocomb@asu.edu>
2026-04-08 22:22:56 +0100

    clean submission

--- build_tools/sub_sol.sh
+++ build_tools/sub_sol.sh
@@ -6,8 +6,8 @@
 #SBATCH -q public
 #SBATCH --time=0-04:00:00
 #SBATCH --mem=0
-#SBATCH -o analysis_fold.out
-#SBATCH -e analysis_fold.out
+#SBATCH -o run.out
+#SBATCH -e run.out
 #SBATCH --export=NONE
 
 ENV_NAME="openmmnqe_custom"
@@ -15,8 +15,4 @@ ENV_NAME="openmmnqe_custom"
 module load mamba/latest
 source activate $ENV_NAME
 
-# Run the cleaner
-# /packages/envs/$ENV_NAME/bin/python3 cleaner.py
-
-# Run the analysis script
-$HOME/.conda/envs/$ENV_NAME/bin/python3 analysis_fold.py ./data/ ./plots/ >> analysis_fold.out 2>&1
+$HOME/.conda/envs/$ENV_NAME/bin/python3 analysis_fold.py ./plots/ >> py.out 2>&1
